d64469a60f38e54e1377a19fc1f7d7aee1193dca
Steven Deshazer <stevendeshazer@tukios.com>
2024-10-10 21:09:55 -0500

    added filepath to lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,8 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
-      theme = 'catppuccin',
+      sections = {
+        lualine_c = { { 'filename', path = 1 } },
+      },
+      options = {
+        theme = 'catppuccin',
+      },
     }
   end,
 }
