3fab8d3f99c5c48d85eeb7b043711956da3d163e
Steven Deshazer <stevendeshazer@tukios.com>
2024-10-10 23:39:43 -0500

    added more keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -79,3 +79,23 @@ vim.keymap.set('n', 'Q', '<nop>')
 
 -- Do a find and replace
 vim.keymap.set('n', '<leader>rp', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'Find and [R]e[P]lace in current file' })
+
+-- Maintain the cursor position when yanking a visual selection.
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+vim.keymap.set('v', 'y', 'myy`y')
+vim.keymap.set('v', 'Y', 'myY`y')
+
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- Reselect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Reselect pasted text
+vim.keymap.set('n', 'p', 'p`[v`]')
+
+-- Easy insertion of a trailing ; or , from insert mode.
+vim.keymap.set('i', ';;', '<Esc>mzA;<Esc>`za')
+vim.keymap.set('i', ',,', '<Esc>mzA,<Esc>`za')
