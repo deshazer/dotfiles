68cf7cf064bb2c5ba08f5f6263592bd498e67915
Steven Deshazer <stevendeshazer@tukios.com>
2024-10-11 01:14:08 -0500

    fixed gx keymap broken by oil.nvim

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,12 +1,27 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
+  {
+    'stevearc/oil.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('oil').setup {
+        default_file_explorer = true,
+      }
+      vim.keymap.set('n', '<leader>-', '<cmd>Oil<cr>', { desc = '[O]il' })
+    end,
   },
-  config = function()
-    require('oil').setup {
-      default_file_explorer = true,
-    }
-    vim.keymap.set('n', '<leader>-', '<cmd>Oil<cr>', { desc = '[O]il' })
-  end,
+  -- oil will break the "open file" behavior of the 'gx' keymap
+  -- So, this plugin will fix it
+  {
+    'chrishrb/gx.nvim',
+    keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
+    cmd = { 'Browse' },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw's gx mapping.
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = true, -- default settings
+    submodules = false, -- submodules are only needed to run tests
+  },
 }
