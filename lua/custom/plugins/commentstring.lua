d75087908431b657418ad9b746f28cedddcd65c8
Steven Deshazer <stevendeshazer@tukios.com>
2024-10-15 15:18:10 -0500

    added treesitter support for blade templates

--- lua/custom/plugins/commentstring.lua
+++ lua/custom/plugins/commentstring.lua
@@ -3,6 +3,11 @@ return {
   config = function()
     require('ts_context_commentstring').setup {
       enable_autocmd = false,
+      custom_calculation = function(node, language_tree)
+        if vim.bo.filetype == 'blade' and language_tree._lang ~= 'javascript' then
+          return '{{-- %s --}}'
+        end
+      end,
     }
   end,
 }
---
+++ lua/kickstart/plugins/treesitter.lua
@@ -0,0 +1,115 @@
+-- Highlight, edit, and navigate code
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = function()
+    require('nvim-treesitter.install').update { with_sync = true }
+  end,
+  dependencies = {
+    { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = {
+      'bash',
+      'comment',
+      'css',
+      'diff',
+      'dockerfile',
+      'git_config',
+      'git_rebase',
+      'gitattributes',
+      'gitcommit',
+      'gitignore',
+      'go',
+      'html',
+      'http',
+      'ini',
+      'javascript',
+      'json',
+      'jsonc',
+      'lua',
+      'make',
+      'markdown',
+      'passwd',
+      'php',
+      'phpdoc',
+      'python',
+      'regex',
+      'rust',
+      'sql',
+      'svelte',
+      'typescript',
+      'vim',
+      'vue',
+      'xml',
+      'yaml',
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+      disable = { 'yaml' },
+    },
+    rainbow = {
+      enable = true,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['if'] = '@function.inner',
+          ['af'] = '@function.outer',
+          ['ia'] = '@parameter.inner',
+          ['aa'] = '@parameter.outer',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ['m'] = '@function.outer',
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['M'] = '@function.outer',
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
+    vim.g.skip_ts_context_commentstring_module = true
+
+    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = 'https://github.com/EmranMR/tree-sitter-blade',
+        files = { 'src/parser.c' },
+        branch = 'main',
+      },
+      filetype = 'blade',
+    }
+    vim.filetype.add {
+      pattern = {
+        ['.*%.blade%.php'] = 'blade',
+      },
+    }
+  end,
+  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+}
