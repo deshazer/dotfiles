173274e2a61764854737b4bdafe5f0ef40d6daa6
Steven Deshazer <stevendeshazer@tukios.com>
2024-10-15 16:38:53 -0500

    improved telescope's git_files options

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -4,6 +4,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'BurntSushi/ripgrep',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
       'nvim-telescope/telescope-fzf-native.nvim',
 
@@ -83,7 +84,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- Make sure every single type of file is searched (including .env files)
       builtin.find_files { find_command = { 'rg', '--files', '--hidden', '--no-ignore', '-u' } }
     end, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search files checked into Git' })
+    vim.keymap.set('n', '<C-p>', function()
+      builtin.git_files { show_untracked = true, use_git_root = true }
+    end, { desc = 'Search files checked into Git' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
