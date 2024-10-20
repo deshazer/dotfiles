4fb25034a0d9170be6dea64017dc531443a6e3eb
Steven Deshazer <stevendeshazer@tukios.com>
2024-10-20 01:18:04 -0500

    basic fn -> function wrap working (still rough draft)

--- lua/custom/plugins/treesj.lua
+++ lua/custom/plugins/treesj.lua
@@ -10,11 +10,14 @@ return {
     vim.keymap.set('n', 'gw', require('treesj').toggle, { desc = '[G]o [W]rap' })
 
     local lang_utils = require 'treesj.langs.utils'
+    local ts_utils = require 'nvim-treesitter.ts_utils'
+    local ts = vim.treesitter
+    -- local treesj = require 'treesj.treesj'
     local php = require 'treesj.langs.php'
 
     local rec_ignore = { 'arguments', 'formal_parameters' }
 
-    -- Teach treejs how to wrap change arrow functions to function () { return } and back
+    -- Teach treeSJ how to wrap change arrow functions to function () { return } and back
     local arrow_body_format_join = function(tsj)
       local parent = tsj:tsnode():parent():type()
       if parent == 'arrow_function' and tsj:tsnode():named_child_count() == 1 then
@@ -59,38 +62,93 @@ return {
             split = {
               recursive_ignore = rec_ignore,
               format_tree = function(tsj)
+                -- I choose not to use the TreeSJ objects because I can't get them to work!!
                 if tsj:type() ~= 'statement_block' then
-                  tsj:wrap { left = '{', right = '}' }
-
-                  local ph = tsj:child 'parenthesized_expression'
-                  local not_seq = ph and not ph:child 'sequence_expression'
-
-                  if not_seq then
-                    if ph:will_be_formatted() then
-                      ph:remove_child { '(', ')' }
-                    else
-                      local text = ph:text():gsub('^%(', ''):gsub('%)$', '')
-                      ph:update_text(text)
-                    end
+                  local node = ts_utils.get_node_at_cursor()
+                  if node == nil then
+                    error 'No Treesitter parser found.'
                   end
 
-                  local body = tsj:child(2)
-                  if body:will_be_formatted() then
-                    local set_return
-                    if body:has_preset 'split' then
-                      set_return = body:child(1)
-                    else
-                      set_return = body:child(1):child(1)
-                    end
-                    set_return:update_text('return ' .. set_return:text() .. ';')
-                  else
-                    body:update_text('return ' .. body:text() .. ';')
+                  while node ~= nil and node:type() ~= 'arrow_function' do
+                    node = node:parent()
                   end
-                  local parent = tsj:parent()
-                  local parent_type = tsj:tsnode():parent():type()
-                  if parent_type == 'arrow_function' then
-                    print(vim.inspect(parent))
+
+                  if node == nil then
+                    error 'No arrow function found.'
                   end
+
+                  local bufnr = vim.api.nvim_get_current_buf()
+
+                  -- This is what we're working with
+                  print(node:type())
+                  print(ts.get_node_text(node, bufnr))
+
+                  local start_row, start_col, end_row, end_col = node:range()
+                  print(start_row, start_col, end_row, end_col)
+
+                  local params_node = node:field('parameters')[1]
+                  local return_type_node = node:field('return_type')[1]
+                  local body_node = node:field('body')[1]
+
+                  print(params_node)
+                  print(return_type_node)
+                  print(body_node)
+
+                  local params_text = params_node and ts.get_node_text(params_node, bufnr) or ''
+                  local return_type_text = return_type_node and ts.get_node_text(return_type_node, bufnr) or ''
+                  local body_text = body_node and ts.get_node_text(body_node, bufnr) or ''
+
+                  print(params_text)
+                  print(return_type_text)
+                  print(body_text)
+
+                  vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, {
+                    'function ' .. params_text .. (return_type_node and (': ' .. return_type_text) or '') .. ' {',
+                    'return ' .. body_text .. ';',
+                    '}',
+                  })
+
+                  require('conform').format { async = false, lsp_format = 'fallback' }
+
+                  -- tsj:wrap { left = '{', right = '}' }
+                  --
+                  -- local ph = tsj:child 'parenthesized_expression'
+                  -- local not_seq = ph and not ph:child 'sequence_expression'
+                  --
+                  -- if not_seq then
+                  --   if ph:will_be_formatted() then
+                  --     ph:remove_child { '(', ')' }
+                  --   else
+                  --     local text = ph:text():gsub('^%(', ''):gsub('%)$', '')
+                  --     ph:update_text(text)
+                  --   end
+                  -- end
+                  --
+                  -- local body = tsj:child(2)
+                  -- if body:will_be_formatted() then
+                  --   local set_return
+                  --   if body:has_preset 'split' then
+                  --     set_return = body:child(1)
+                  --   else
+                  --     set_return = body:child(1):child(1)
+                  --   end
+                  --   set_return:update_text('return ' .. set_return:text() .. ';')
+                  -- else
+                  --   body:update_text('return ' .. body:text() .. ';')
+                  -- end
+                  -- local parent = tsj:tsnode():parent()
+                  -- print(vim.inspect(tsj))
+                  -- local parent_type = parent:type()
+                  -- if parent_type == 'arrow_function' then
+                  --   local parent_tsj = require('treesj.treesj').new { tsnode = parent, lang = 'php', from_self = false }
+                  --   local new_text = parent_tsj:text():gsub('fn', 'function', 1):gsub('=>', '', 1)
+                  --
+                  --   -- parent_tsj:update_text(parent_tsj:text():gsub('fn', 'function', 1):gsub('=>', '', 1))
+                  --   -- print(parent_tsj:text())
+                  --   -- print(new_text)
+                  --   parent_tsj:update_text(new_text)
+                  --   print(vim.inspect(parent_tsj))
+                  -- end
                 end
               end,
             },
@@ -100,7 +158,7 @@ return {
               format_tree = arrow_body_format_join,
             },
           },
-          arrow_function = { target_nodes = { 'body' } },
+          arrow_function = { target_nodes = { 'arrow_function', 'body' } },
         }),
       },
     }
